5452cd08e411ecef4d505d48e06f285cfddf7f54
Huy <kh.nguyen.huy@gmail.com>
2022-12-04 21:13:37 +0100

    remove logs

--- day 4/day_4_2.js
+++ day 4/day_4_2.js
@@ -16,14 +16,6 @@ const run = () => {
     const dataByLine = data.split("\n");
     let numberPair = 0;
 
-    console.log(isAssignementOverlapping("2-4,6-8"));
-    console.log(isAssignementOverlapping("2-3,4-5"));
-
-    console.log(isAssignementOverlapping("5-7,7-9"));
-    console.log(isAssignementOverlapping("2-8,3-7"));
-    console.log(isAssignementOverlapping("6-6,4-6"));
-    console.log(isAssignementOverlapping("2-6,4-8"));
-
     dataByLine.forEach((pair) => {
       numberPair += isAssignementOverlapping(pair) ? 1 : 0;
     });
